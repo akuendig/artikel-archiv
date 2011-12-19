5e1eecf6bd5f945740b03d6c365d721c3378b547
Adrian Kuendig <adriankue@gmail.com>
2011-12-19 14:02:38 +0100

    working on stack-downloader

---
+++ model.js
@@ -0,0 +1,14 @@
+﻿var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
+
+var Article = mongoose.model('Article', new Schema({
+    id: String,
+    title: String,
+    summary: String,
+    pubDate: Date,
+    link: String,
+    site: String
+}));
+
+exports.Article = Article;
